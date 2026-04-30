41001b278bf59c213ff5a50fb200f3e95491ad70
mgongar1508 <manologonzalezgarcia64@gmail.com>
2026-04-30 18:14:32 +0200

    style fix

--- resources/views/components/dialog-modal.blade.php
+++ resources/views/components/dialog-modal.blade.php
@@ -3,19 +3,19 @@
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
 
     <!-- HEADER -->
-    <div class="px-6 py-4 bg-[#1a1a1b] border-b border-gray-800 rounded-t-xl">
+    <div class="px-6 py-4 dark:bg-[#0f0f10] border-b border-gray-800 rounded-t-xl">
         <h2 class="text-lg font-semibold text-gray-100">
             {{ $title }}
         </h2>
     </div>
 
     <!-- CONTENT -->
-    <div class="px-6 py-5 bg-[#1a1a1b] text-gray-300">
+    <div class="px-6 py-5 dark:bg-[#0f0f10] text-gray-300">
         {{ $content }}
     </div>
 
     <!-- FOOTER -->
-    <div class="flex justify-end gap-3 px-6 py-4 bg-[#1a1a1b] border-t border-gray-800 rounded-b-xl">
+    <div class="flex justify-end gap-3 px-6 py-4 dark:bg-[#0f0f10] border-t border-gray-800 rounded-b-xl">
         {{ $footer }}
     </div>
 
